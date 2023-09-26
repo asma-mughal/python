c7348ed1327e1e93ee5a6b597c237636e35f65ba
unknown <asmamughal097@gmail.com>
2023-09-26 17:56:25 +0500

    first commit

---
+++ variables.py
@@ -0,0 +1,6 @@
+failed_subjects = "6" #failed subjects gets the number 6.
+name = 'John'
+print('Your '+ name + ' failed '+failed_subjects +'subjects')
+print(name+ ' will need to redo' +failed_subjects+ 'courses.')
+name = "Eric"
+print(name+  ' is doing well in geography.')
