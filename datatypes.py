36c43fcbadc99e19fea816b18ad6fd8a9a598518
unknown <asmamughal097@gmail.com>
2023-09-26 18:27:40 +0500

    2nd

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+#1. String, integer, decimal, boolean
+failed_subjects = "6" #failed subjects gets the number 6.
+name = 'John'
+is_going_to_Party = True
+a = "it's"
+print('Your '+ name + ' failed '+failed_subjects +'subjects')
+print(name+ ' will need to redo' +failed_subjects+ 'courses.')
+name = "Eric"
+print(name+  ' is doing well in geography.')
+c1 = int("3.4") #decimal string will not convert into integer.
+print (c1)
