f50134f9d23f681059e724f91853ec46dca02818
unknown <asmamughal097@gmail.com>
2023-09-27 15:21:32 +0500

    day3

---
+++ arthemetic.py
@@ -0,0 +1,9 @@
+a= 6
+b = 2
+print("Addition",  a+b)
+print("Sub",  a-b)
+print("Mul",  a*b)
+print("Div(float)",  a/b)
+print("Div(floor)",  a//b)
+print("Exponent",  a**b)
+print("Mod",a%b)
--- datatypes.py
+++ datatypes.py
@@ -7,5 +7,10 @@ print('Your '+ name + ' failed '+failed_subjects +'subjects')
 print(name+ ' will need to redo' +failed_subjects+ 'courses.')
 name = "Eric"
 print(name+  ' is doing well in geography.')
-c1 = int("3.4") #decimal string will not convert into integer.
-print (c1)
+#c1 = int("3.4") #decimal string will not convert into integer.
+
+c1 = int(float("3.4"))
+print (c1) 
+name_of_thing = "widget"
+price = 12.9
+num_in_stock = 34
