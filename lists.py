a9e2e32b3c6b67717977c54225503a3b93d5624e
unknown <asmamughal097@gmail.com>
2023-10-04 17:50:54 +0500

    first

---
+++ lists.py
@@ -0,0 +1,50 @@
+#All built-In Functions of List
+friends = ["Michael", "John", "Terry", "eric", 1, True]
+print(friends[2:4])
+print(len(friends))
+print(friends.index(True))
+print(friends.count('Eric'))
+#you cannot sort different datatypes
+life = ["Failure", "Short", "Tuff", "Depressed"]
+another_life = ["Sedness", "Alone"]
+# (life.sort())
+# print(life)
+# print(life.sort(reverse=True))
+# print(life)
+print(life.reverse())
+
+print(life)
+print(max(life))
+#Modifying Lists
+life.append("Patience")
+print(life)
+life.insert(2, "ANGER")
+print(life)
+life[3]= "Most Tuff"
+print(life)
+life.extend(another_life)
+
+print(life)
+#remove, pop, delete
+life.pop(-1)
+print(life)
+
+life.clear()
+print(life)
+del life #I AM DED
+
+#Copying lists
+new_life = friends[:]
+print(new_life)
+new_friends = list(friends)
+number_of_sold_week1 = [12, 34, 67,89]
+number_of_sold_week2 = [14,5,1,2,9]
+sales =[]
+another_day = int(input("Enter sales for another day: "))
+number_of_sold_week2.append(another_day)
+print(number_of_sold_week2)
+sales.extend(number_of_sold_week1)
+sales.extend(number_of_sold_week2)
+print("Worst Day: " ,min(sales))
+print("Best Day: ", max(sales))
+print("Total: " ,sum(sales))
