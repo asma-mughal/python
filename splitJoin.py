9e0f82a51ec7906ce063876591a62fdb7377c9ac
unknown <asmamughal097@gmail.com>
2023-10-05 22:46:25 +0500

    consistent

---
+++ splitJoin.py
@@ -0,0 +1,16 @@
+# msg ='Welcome to Python 101: Split and Join'
+# csv = 'Eric,John,Michael,Terry,Graham'
+# friends_list = ['Eric','John','Michael','Terry','Graham']
+# #CONVERT STRING -> LIST (SPLIT)
+# print(msg.split())
+# print(csv.split(','))
+# #CONVERT LIST -> STRING (JOIN)
+# print(','.join(friends_list + csv.split(',')))
+# print(' '.join(msg.split(' ')))
+
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+friends_list = ['Exercise: fill me with names']
+new_string = (''.join( friends_list))
+print(new_string)
+print(new_string.split(','))
+print(','.join(','.join(csv.split(';')).split(':')))
